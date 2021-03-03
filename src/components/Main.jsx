8c4c06ff48d199016c2edbebdfe4847ebf22040b
Frank Stepanski <fsjay@hotmail.com>
2021-03-03 15:44:49 -0500

    updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,104 +8,62 @@ function Main() {
         <>
          <VerticalTimeline layout={'1-column-right'}>
           <VerticalTimelineElement
-             className=""
-             contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-           
-           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+               className=""
+               contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+               iconStyle={{ background: 'rgb(33, 150, 243)', color: '#2196f3' }}
           >
           <h3 className="">Coding Bootcamp Topic Guide</h3>
-        
-          <p>
-         
-          </p>
+          <p>TBA</p>
         </VerticalTimelineElement>
-      <VerticalTimelineElement
-    className=""
-   
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    
-  >
-    <h3 className="vertical-timeline-element-title">Computer Science</h3>
-    <p>
-   TBA
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-   
-  >
-    <h3 className="vertical-timeline-element-title">Browser Based Technologies</h3>
-    <p>
-    TBA
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    
-  >
-    <h3 className="vertical-timeline-element-title">Deployment</h3>
-    <p>
-      TBA
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="April 2013"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-  >
-    <h3 className="">Databases</h3>
-    <p>
-     TBA
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className=""
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    
-  >
-    <h3 className="">Server-side Development</h3>
-    <p>
-      TBA
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className=""
- 
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-   
-  >
-    <h3 className="">Quality and Performance</h3>
-    <p>
-      TBA
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-   
-  />
-   <VerticalTimelineElement
-    className=""
- 
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-   
-  >
-    <h3 className="">Agile Management</h3>
-    <p>
-      TBA
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-   
-  />
-</VerticalTimeline>
-        </>
-      );
+        <VerticalTimelineElement
+              className=""
+              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+            <h3 className="vertical-timeline-element-title">Computer Science</h3>
+            <p>TBA</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+             className="vertical-timeline-element--work"
+             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+             <h3 className="vertical-timeline-element-title">Browser Based Technologies</h3>
+             <p>TBA</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+             className="vertical-timeline-element--work"
+             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+            <h3 className="vertical-timeline-element-title">Deployment</h3>
+            <p>TBA</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+            className="vertical-timeline-element--topic"
+            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+        >
+            <h3 className="">Databases</h3>
+            <p>TBA</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+             iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+        >
+             <h3 className="">Server-side Development</h3>
+             <p>TBA</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+             iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+         >
+             <h3 className="">Quality and Performance</h3>
+             <p>TBA</p>
+         </VerticalTimelineElement>
+        <VerticalTimelineElement
+               iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+        >
+               <h3 className="">Agile Management</h3>
+              <p>TBA</p>
+       </VerticalTimelineElement>
+     </VerticalTimeline>
+    </>
+   );
 }
   
 export default Main;
