c9b9c16f337792fbf3150dd5f8915d4f7e7568a4
Frank Stepanski <fsjay@hotmail.com>
2021-03-02 22:00:03 -0500

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,10 @@ module.exports =  {
                 test: /\.(png|jpe?g|gif|svg)$/i,
                 use: [
                   {
-                    loader: 'url-loader'
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'images',
+                      }
                   },
                 ],
             },
